59538d9ac0af6219a8a7898a25623cd84c68dbad
Zulfqor <ex3ero@gmail.com>
2026-08-16 15:13:34 +0500

    Set base path for Vite configuration
    done

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend/',
   plugins: [react(), tailwindcss()],
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
